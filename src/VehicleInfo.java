f341ec381baa4d93b3758126eb375bd71471fd2a
KushaalPatel <kushaal.patel@uconn.edu>
2021-10-13 00:11:24 -0400

    added comments

--- src/VehicleInfo.java
+++ src/VehicleInfo.java
@@ -1,9 +1,11 @@
 public class VehicleInfo {
+    // private vehicleinfo variables
     private String model; // name of the vehicle model
     private String year; // year in which the vehicle was manufactured
     private String type; // type of vehicle, must select from: sedan, hatchback, luxury, sport, other
     private String method; // how the vehical was acquired -- must select from: new certified pre-owned,
                            // used, other
+    // construct class
 
     public VehicleInfo(String vehicle_model, String vehicle_year, String vehicle_type, String vehicle_method) {
         model = vehicle_model;
@@ -12,34 +14,42 @@ public class VehicleInfo {
         method = vehicle_method;
     }
 
+    // get model
     public String getModel() {
         return model;
     }
 
+    // get year
     public String getYear() {
         return year;
     }
 
+    // get type
     public String getType() {
         return type;
     }
 
+    // get method
     public String getMethod() {
         return method;
     }
 
+    // update model
     public void updateModel(String NewModel) {
         model = NewModel;
     }
 
+    // update year
     public void updateYear(String NewYear) {
         year = NewYear;
     }
 
+    // update type
     public void updateType(String NewType) {
         type = NewType;
     }
 
+    // update method
     public void updateMethod(String NewMethod) {
         method = NewMethod;
     }
