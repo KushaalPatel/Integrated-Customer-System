84a8785a4b0f923b06f772208210a965f09c69f9
Kushaal <krazykushaal@gmail.com>
2021-10-13 22:15:23 -0400

    Started

--- src/CustomerProfDB.java
+++ src/CustomerProfDB.java
@@ -1,2 +1,11 @@
 public class CustomerProfDB {
+    private int numCustomer, currentCustomerIndex;
+    private String fileName;
+    private CustomerProf[] customerList;
+
+    public CustomerProfDB(String whatfileName){
+        fileName = whatfileName;
+        numCustomer = 0; // init to 0
+        currentCustomerIndex = 0; // init to 0
+    }
 }
